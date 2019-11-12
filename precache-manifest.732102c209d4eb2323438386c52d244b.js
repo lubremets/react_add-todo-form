bce548ca7393292c92dabf739c9c7e31bc637fb9
lubremets <lubremets@gmail.com>
2019-11-12 13:36:14 +0200

    Updates

---
+++ precache-manifest.732102c209d4eb2323438386c52d244b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb821c81723030463192cb768100ddf9",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "8171192e929cb51ba0d7",
+    "url": "/react_add-todo-form/static/css/main.9b7c3ff9.chunk.css"
+  },
+  {
+    "revision": "9cef1aa7831a1d42b365",
+    "url": "/react_add-todo-form/static/js/2.5120375e.chunk.js"
+  },
+  {
+    "revision": "8171192e929cb51ba0d7",
+    "url": "/react_add-todo-form/static/js/main.60862d92.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
